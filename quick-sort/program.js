23993945332ed1c46f026fa2c9dbc00564453b86
gabriela <gabipilegi@gmail.com>
2019-03-24 19:16:59 -0300

    sort and count comparissons

--- quick-sort/program.js
+++ quick-sort/program.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 
-
 function swap(input, i, j) {
     let aux = input[i]
     input[i] = input[j]
@@ -8,7 +7,7 @@ function swap(input, i, j) {
 }
 
 function partition(input, l, r) {
-    console.log(input.slice(l, r + 1))
+
     if (r == l + 1 && input[l] < input[r]) return l
     let pivot = input[l]
     let i = l + 1
@@ -23,12 +22,38 @@ function partition(input, l, r) {
     return switchPosition
 }
 
-function sort(input, l, r) {
+function compare(a, b) {
+    if (a.value > b.value) return 1;
+    if (a.value < b.value) return -1;
+    return 0;
+}
+
+function medianOfThreePivod(input, l, r) {
+    let positions = [{ index: l, value: input[l] },
+    { index: r, value: input[r] }]
+
+    if (input.length > 2) {
+        let mid = Math.floor((r - l) / 2)
+        positions.push({ index: mid, value: input[mid] })
+    }
+    return positions.sort(compare)[1].index
+}
+
+function choosePivot(strategy, input, l, r) {
+    switch (strategy) {
+        case 1: swap(input, l, r)
+            break
+        case 2: swap(input, l, medianOfThreePivod(input, l, r))
+    }
+}
+
+function sortAndCountComparisons(input, l, r, strategy) {
     if (r == l) return 0
+    choosePivot(strategy, input, l, r)
     let i = partition(input, l, r)
     let levelComp = (r - l)
-    let leftComp = sort(input, l, Math.max(l, i - 1))
-    let rightComp = sort(input, Math.min(r, i + 1), r)
+    let leftComp = sortAndCountComparisons(input, l, Math.max(l, i - 1), strategy)
+    let rightComp = sortAndCountComparisons(input, Math.min(r, i + 1), r, strategy)
     return levelComp + leftComp + rightComp
 }
 function getInput() {
@@ -41,7 +66,11 @@ function getInput() {
 
 function doTheStuff() {
     let input = getInput()
-    return sort(input, 0, input.length - 1)
+    return sortAndCountComparisons(input, 0, input.length - 1, 2)
 }
-//console.log(partition([5,2,1],0,2))
+
 console.log(doTheStuff())
+
+//0: 162085
+//1: 164123
+//2: 159894
